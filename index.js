52782d533077d2a85685bbde162d51eb08456bff
nandakamila <nandarizkamila@gmail.com>
2024-01-31 21:36:23 +0700

    feat: UPDATE EXERCISE

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-console.log(`Hello World`);
+/*
+● Write a code to display the multiplication table of a given integer.
+○ Example : Number → 9
+○ Output : 
+            ■ 9 x 1
+            ■ 9 x 2
+            ■ …
+            ■ 9 x 10
+
+*/
+let numberInteger=9;
+for(i=1;i<=10;i++){
+    console.log(`${numberInteger} x ${i} = ${numberInteger*i}`);
+}
+/*
+● Write a code to check whether a string is a palindrome or not.
+○ Example : ‘madam’ → palindrome
+*/
+let isPalindrome;
+
+/*
+● Write a code to convert centimeter to kilometer.
+○ Example : 100000 → “1 km”
+*/
+centimeter=100000
+let kilometer = centimeter/100000
+console.log(`${centimeter} = ${kilometer}`);
+/*
+
+● Write a code to format number as currency (IDR)
+○ Example : 1000 → “Rp. 1.000,00”
+*/
+numberCurrency=1000;
+
+/*
+● Write a code to remove the first occurrence of a given “search string” from a string
+○ Example : string = “Hello world”, search string = “ell” → “Ho world”
+*/
+
+/*
+● Write a code to capitalize the first letter of each word in a string
+○ Example : “hello world” → “Hello World”
+*/
+
+/*
+● Write a code to reverse a string.
+○ Example : “hello” → “olleh”
+
+*/
